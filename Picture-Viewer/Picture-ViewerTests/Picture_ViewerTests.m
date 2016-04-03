c9452c38fbab4e61f27f2f38a31f0721d0b5e367
susu <28025920@qq.com>
2016-04-03 10:54:59 +0800

    创建了项目
    创建了项目

---
+++ Picture-Viewer/Picture-ViewerTests/Picture_ViewerTests.m
@@ -0,0 +1,39 @@
+//
+//  Picture_ViewerTests.m
+//  Picture-ViewerTests
+//
+//  Created by  江苏 on 16/4/3.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Picture_ViewerTests : XCTestCase
+
+@end
+
+@implementation Picture_ViewerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
